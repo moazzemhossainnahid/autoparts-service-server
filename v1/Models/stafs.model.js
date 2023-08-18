6a1675165538668df33415f27797221679513966
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-08-18 15:34:05 +0600

    change stafs model

--- v1/Models/stafs.model.js
+++ v1/Models/stafs.model.js
@@ -4,13 +4,31 @@ const validator = require("validator");
 
 const stafSchema = mongoose.Schema(
     {
-        id: { type: String },
-        name: { type: String },
-        brand: { type: String },
-        description: { type: String },
-        price: { type: String },
-        image: { type: String },
-        cartQuantity: { type: Number },
+        name: {
+            type: String,
+            trim: true,
+            unique: false,
+            required: [true, "Name is required"],
+        },
+        age: {
+            type: String,
+            required: [true, "Age is required"],
+            trim: true,
+        },
+        image: {
+            required: true,
+            type: String,
+            validate: [validator.isURL, "Please provide Product Image URL"],
+        },
+        experience: {
+            type: String,
+            required: [true, "Experience is required"],
+        },
+        work_name: {
+            type: String,
+            required: [true, "Work Name is required"],
+        },
+
     },
     {
         timestamps: true,
