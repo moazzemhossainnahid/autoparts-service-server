a084c387336f839eda449e835f5316d9bc96b0f0
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-08-26 21:48:36 +0600

    resolved

--- v1/Models/orders.model.js
+++ v1/Models/orders.model.js
@@ -1,35 +1,49 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
- 
 const orderSchema = mongoose.Schema(
-    {
-        userId: { type: String, required: true },
-        customerId: { type: String, required: true },
-        paymentIntentId: { type: String, required: true },
-        products: [
-            {
-                id: { type: String },
-                name: { type: String },
-                brand: { type: String },
-                description: { type: String },
-                price: { type: String },
-                image: { type: String },
-                cartQuantity: { type: Number },
-            }
-        ],
-        subtotal: { type: Number, required: true },
-        total: { type: Number, required: true },
-        shipping: { type: Object, required: true },
-        delivery_status: { type: String, default: "pending" },
-        payment_status: { type: String, required: true },
-    },
-    {
-        timestamps: true,
-    }
-
+  {
+    total_amount: String,
+    currency: String,
+    tran_id: String,
+    success_url: String,
+    fail_url: String,
+    cancel_url: String,
+    paymentStatus: String,
+    shipping_method: String,
+    product_name: String,
+    product_category: String,
+    product_profile: String,
+    product_image: String,
+    cus_name: String,
+    cus_email: String,
+    cus_add1: String,
+    cus_add2: String,
+    cus_city: String,
+    cus_state: String,
+    cus_postcode: Number,
+    cus_country: String,
+    cus_phone: String,
+    cus_fax: String,
+    ship_name: String,
+    ship_add1: String,
+    ship_add2: String,
+    ship_city: String,
+    ship_state: String,
+    ship_postcode: Number,
+    ship_country: String,
+    multi_card_name: String,
+    value_a: String,
+    value_b: String,
+    value_c: String,
+    value_d: String,
+    ipn_url: String,
+    val_id: String,
+  },
+  {
+    timestamps: true,
+  }
 );
-
 
 const Orders = mongoose.model("orders", orderSchema);
 
